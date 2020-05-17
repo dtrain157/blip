59d0e408db5560f13f6d270df781c8ecd0497e86
Derrick van Zyl <derrick.vanzyl@gmail.com>
2020-05-17 15:15:01 +0100

    .gitignore changes

--- lib/glad-gl/target/rls/debug/deps/glad_gl-3140307a9c589dc8.d
+++
@@ -1,6 +0,0 @@
-c:\Users\Derrick\OneDrive\PERM\CodeBase\Rust\playground\glad-gl\target\rls\debug\deps\glad_gl-3140307a9c589dc8.rmeta: src\lib.rs src\gl.rs
-
-c:\Users\Derrick\OneDrive\PERM\CodeBase\Rust\playground\glad-gl\target\rls\debug\deps\glad_gl-3140307a9c589dc8.d: src\lib.rs src\gl.rs
-
-src\lib.rs:
-src\gl.rs:
